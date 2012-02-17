298d6a50bee8dac6368881d75286d32dd24c9568
ap4y <lod@pisem.net>
2012-02-17 15:27:13 +0300

    initial commit

---
+++ iMagickWand/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iMagickWand
+//
+//  Created by Arthur Evstifeev on 2/17/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
